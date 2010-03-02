1df60c9439a662762f5f116ef06c983233d07888
Dan Mohl <dmohl@yahoo.com>
2010-03-02 13:51:14 -0600

    Created

---
+++ TDD_Example/src/DependencyProvider.cs
@@ -0,0 +1,18 @@
+﻿using StructureMap;
+
+namespace TDD_Example.Calculator
+{
+    public class DependencyProvider
+    {
+        public static void Initialize()
+        {
+
+            ObjectFactory.Configure(x => x.Scan(scanner =>
+                                                    {
+                                                        scanner.TheCallingAssembly();
+                                                        scanner.WithDefaultConventions();
+                                                    }));
+            
+        }
+    }
+}
---
+++ TDD_Example/src/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TDD_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
